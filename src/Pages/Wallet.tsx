34eae2df0dc213bf544755f96d04d0a10f993b4a
Luis Aneudy <dluisaneudy82@gmail.com>
2023-09-15 14:24:52 -0400

    Fixing card

--- src/Pages/Wallet.tsx
+++ src/Pages/Wallet.tsx
@@ -1,5 +1,24 @@
 export default function Wallet() {
   return (
-    <div>Wallet</div>
+    <section>
+      <table className="bg-second p-4">
+        <thead>
+
+          <tr>
+
+            <th>
+              <button>Add</button>
+            </th>
+
+            <th>Swap</th>
+
+            <th>Value</th>
+
+            <th>Price</th>
+          </tr>
+        </thead>
+
+      </table>
+    </section>
   )
 }
